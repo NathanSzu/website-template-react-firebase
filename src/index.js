7072546ed6f6270911ecce0ff389d32493e78cb6
Nathan Szurek <nathan.szurek@yahoo.com>
2021-02-28 09:40:43 -0600

    Added firebase config to index,js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyAmDYLiqikrosA0IjB_m_pZWaR0krPd7q8",
+  authDomain: "website-templates-92f58.firebaseapp.com",
+  projectId: "website-templates-92f58",
+  storageBucket: "website-templates-92f58.appspot.com",
+  messagingSenderId: "688175359553",
+  appId: "1:688175359553:web:d2e2ddefcb9160788b48c0"
+}
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
